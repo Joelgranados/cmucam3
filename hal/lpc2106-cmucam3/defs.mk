091790a58c8e96b01e142c0f083b6d44905506dc
agoode <agoode@111db651-790d-0410-a044-b0abe44a5820>
2006-02-08 19:42:40 +0000

    make
    git-svn-id: https://cc3.svn.sourceforge.net/svnroot/cc3/trunk@126 111db651-790d-0410-a044-b0abe44a5820

--- hal/lpc2106-cmucam3/defs.mk
+++ hal/lpc2106-cmucam3/defs.mk
@@ -20,8 +20,8 @@ ifeq ($(strip $(thumb)),1)
   THUMB_SUFFIX=$(DASH_THUMB)
 endif
 
-LIBS=
-CFLAGS=-I$(HALDIR) -I$(HALDIR)/../../include -Os -mcpu=arm7tdmi -Wall -Wstrict-prototypes -Wcast-align -Wcast-qual -Wimplicit -Wmissing-declarations -Wmissing-prototypes -Wnested-externs -Wpointer-arith -Wswitch -Wredundant-decls -Wreturn-type -Wshadow -Wstrict-prototypes -Wunused -Wextra -Werror-implicit-function-declaration -ffreestanding -std=gnu99 -g -fdata-sections -ffunction-sections -msoft-float $(THUMB_FLAGS)
-LDFLAGS=-nostartfiles -lm -T$(HALDIR)/arm.ln -Wl,--gc-sections -mcpu=arm7tdmi -msoft-float $(THUMB_FLAGS)
+LIBS+=
+override CFLAGS+=-I$(HALDIR) -I$(HALDIR)/../../include -Os -mcpu=arm7tdmi -Wall -Wstrict-prototypes -Wcast-align -Wcast-qual -Wimplicit -Wmissing-declarations -Wmissing-prototypes -Wnested-externs -Wpointer-arith -Wswitch -Wredundant-decls -Wreturn-type -Wshadow -Wstrict-prototypes -Wunused -Wextra -Werror-implicit-function-declaration -ffreestanding -std=gnu99 -g -fdata-sections -ffunction-sections -msoft-float $(THUMB_FLAGS)
+override LDFLAGS+=-nostartfiles -lm -T$(HALDIR)/arm.ln -Wl,--gc-sections -mcpu=arm7tdmi -msoft-float $(THUMB_FLAGS)
 
 HALLIB=libhal-$(HALNAME).a
