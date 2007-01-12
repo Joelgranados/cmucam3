61a816851ab74201b9c15f2e3096f40d5b197070
goodea <goodea@111db651-790d-0410-a044-b0abe44a5820>
2007-01-12 16:30:32 +0000

    space
    git-svn-id: https://cc3.svn.sourceforge.net/svnroot/cc3/trunk@289 111db651-790d-0410-a044-b0abe44a5820

--- projects/ppm_grab/main.c
+++ projects/ppm_grab/main.c
@@ -9,22 +9,21 @@ int main(void) {
   int i;
   FILE *f;
 
-  // setup system    
-  cc3_system_setup ();
-  cc3_uart_init (0, 
+  // setup system
+  cc3_system_setup();
+  cc3_uart_init (0,
 		 CC3_UART_RATE_115200,
 		 CC3_UART_MODE_8N1,
 		 CC3_UART_BINMODE_TEXT);
 
   cc3_camera_init ();
-   
+
  // cc3_set_colorspace(CC3_YCRCB);
   cc3_set_resolution(CC3_HIGH_RES);
   // cc3_pixbuf_set_subsample (CC3_NEAREST, 2, 2);
   cc3_wait_ms(1000);
 
-  // init jpeg
-
+  // init
   cc3_set_led(1);
   i = 0;
   while(true) {
@@ -32,41 +31,37 @@ int main(void) {
     cc3_clr_led(1);
     while(!cc3_read_button());
     cc3_set_led(1);
-  
-   // Check if files exist, if they do then skip over them 
-    do { 
-    	snprintf(filename, 16, "c:/img%.5d.ppm", i);
-    	//snprintf(filename, 16, "img%.5d.ppm", i);
-    	f = fopen(filename, "r");
-    	if(f!=NULL ) { 
-		printf( "%s already exists...\n",filename ); 
-		i++; 
-		fclose(f);
-		}
-    } while(f!=NULL);
+
+    // Check if files exist, if they do then skip over them
+    do {
+      snprintf(filename, 16, "c:/img%.5d.ppm", i);
+      //snprintf(filename, 16, "img%.5d.ppm", i);
+      f = fopen(filename, "r");
+      if (f != NULL) {
+	printf( "%s already exists...\n",filename );
+	i++;
+	fclose(f);
+      }
+    } while(f != NULL);
 
     // print file that you are going to write to stderr
     fprintf(stderr,"%s ", filename);
     fflush(stderr);
     f = fopen(filename, "w");
-    if(f==NULL || i>200 )
-    {
-	cc3_set_led(2);
-	while(1);
+    if(f == NULL || i > 200) {
+      cc3_set_led(2);
+      while(1);
     }
     capture_ppm(f);
 
     fclose(f);
     fprintf(stderr, "\r\n");
-    
+
     i++;
   }
-  
 
   return 0;
 }
-
-
 
 
 void capture_ppm(FILE *f)
