4c81f4a9ff7037ed3ea2aeb2a36ad56bc6122d6c
goodea <goodea@111db651-790d-0410-a044-b0abe44a5820>
2007-01-15 05:07:54 +0000

    copyright
    git-svn-id: https://cc3.svn.sourceforge.net/svnroot/cc3/trunk@331 111db651-790d-0410-a044-b0abe44a5820

--- lib/cc3_ilp/cc3_ilp.c
+++ lib/cc3_ilp/cc3_ilp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2007  Anthony Rowe
+ * Copyright 2006-2007  Anthony Rowe and Adam Goode
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
