92e0eb0ed2ffbfda7a5c0e8afd103bdcc8e08982
anthony_rowe <anthony_rowe@111db651-790d-0410-a044-b0abe44a5820>
2007-07-29 20:28:21 +0000

    added simple-histogram and histogram-match
    git-svn-id: https://cc3.svn.sourceforge.net/svnroot/cc3/trunk@526 111db651-790d-0410-a044-b0abe44a5820

---
+++ projects/histogram-match/README.txt
@@ -0,0 +1,22 @@
+About
+---------
+
+This is a sample program that shows how you can use histogram matching to
+distinguish between images.  This is a very simple form of template based
+object recognition.  In order for this approach to work well the scene needs
+to have consistent positioning as well as constant lighting. These types of
+strict assumptions are typically found in inspection applications where the
+camera, objects and lighting are all fixed.
+
+
+Operation
+---------
+Load the histogram-match firmware.  The firmware is set to grab the first
+TEMPLATE_IMGS (by default 4) images as template images to compare with
+future images.  Upon startup, press the button each time you want to train
+a new template image.  Once all templates have been filled, the program
+will continously grab frames and compare them to the templates. 
+
+By default the region of interest in the image is set to (160, 50, 200, 240)
+but can be easily changed.
+
---
+++ projects/histogram-match/main.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cc3.h>
+#include <cc3_ilp.h>
+#include <cc3_histogram.h>
+
+#define TEMPLATE_IMGS     4
+#define DETECT_THRESH	  2000
+
+void simple_get_histogram (cc3_histogram_pkt_t * h_pkt);
+uint32_t histogram_subtract (cc3_histogram_pkt_t * h1,
+                             cc3_histogram_pkt_t * h2);
+
+int main (void)
+{
+  cc3_histogram_pkt_t my_hist;
+  cc3_histogram_pkt_t train_hist[TEMPLATE_IMGS];
+  uint32_t i;
+
+  cc3_uart_init (0,
+                 CC3_UART_RATE_115200,
+                 CC3_UART_MODE_8N1, CC3_UART_BINMODE_TEXT);
+
+  cc3_camera_init ();
+
+
+  //cc3_camera_set_colorspace(CC3_COLORSPACE_YCRCB);
+  cc3_camera_set_resolution (CC3_CAMERA_RESOLUTION_HIGH);
+  //cc3_pixbuf_frame_set_subsample(CC3_SUBSAMPLE_NEAREST, 2, 2);
+
+  // Set a region on the image where we take a histogram.
+  // This could be the entire image, or just a section.
+  cc3_pixbuf_frame_set_roi (160, 50, 200, 240);
+
+  // init pixbuf with width and height
+  // When using the virtual-cam, note that this will skip the first image
+  cc3_pixbuf_load ();
+
+  my_hist.channel = CC3_CHANNEL_GREEN;
+  my_hist.bins = 24;
+  my_hist.hist = malloc (my_hist.bins * sizeof (uint32_t));
+
+  cc3_led_set_state (1, 0);
+  // Read in the next TEMPLATE_IMGS number of images to build templates
+  for (i = 0; i < TEMPLATE_IMGS; i++) {
+    cc3_led_set_state (0, 0);
+    printf( "Press button to train image %d...\n",i );
+    // Wait for button press to capture template image
+    while (!cc3_button_get_state ());
+    cc3_led_set_state (0, 1);
+    train_hist[i].channel = CC3_CHANNEL_GREEN;
+    train_hist[i].bins = 24;
+    train_hist[i].hist = malloc (train_hist[i].bins * sizeof (uint32_t));
+    simple_get_histogram (&train_hist[i]);
+    printf( "Image loaded...\n" );
+  }
+  // Turn on LED when training done
+  cc3_led_set_state (0, 0);
+  cc3_led_set_state (1, 1);
+
+  while (true) {
+    uint32_t diff, min;
+    int8_t template;
+
+    // Grab an image and take a histogram of it
+    simple_get_histogram (&my_hist);
+
+    // Print the histogram out on the screen
+    printf ("hist: ");
+    for (i = 0; i < my_hist.bins; i++)
+      printf ("%d ", my_hist.hist[i]);
+    printf ("\n");
+
+    // Look for the min absolute difference between the new image and the templates
+    min = 0xFFFFFFFF;
+    template = -1;
+    for (i = 0; i < TEMPLATE_IMGS; i++) {
+      diff = histogram_subtract (&my_hist, &train_hist[i]);
+      if (diff < min) {
+        min = diff;
+        template = i;
+      }
+    }
+
+    if (min < DETECT_THRESH) {
+      cc3_led_set_state (0, 1);
+      printf ("Image matched template %d with value %d\n", template, min);
+    }
+    else {
+      cc3_led_set_state (0, 0);
+      printf ("NO IMAGE MATCHED: closest template %d with value %d\n",
+              template, min);
+    }
+  }
+
+}
+
+uint32_t histogram_subtract (cc3_histogram_pkt_t * h1,
+                             cc3_histogram_pkt_t * h2)
+{
+  uint32_t diff;
+  uint32_t i;
+
+  if (h1->bins != h2->bins) {
+    printf ("histogram_subtract error:  bin size mismatch\n");
+    return 0;
+  }
+
+  diff = 0;
+  for (i = 0; i < h1->bins; i++) {
+    if (h1->hist[i] > h2->hist[i])
+      diff += h1->hist[i] - h2->hist[i];
+    else
+      diff += h2->hist[i] - h1->hist[i];
+  }
+  return diff;
+}
+
+void simple_get_histogram (cc3_histogram_pkt_t * h_pkt)
+{
+  cc3_image_t img;
+
+  img.channels = 3;
+  img.width = cc3_g_pixbuf_frame.width;
+  img.height = 1;               // image will hold just 1 row for scanline processing
+  img.pix = cc3_malloc_rows (1);
+  if (img.pix == NULL) {
+    return;
+  }
+
+  cc3_pixbuf_load ();
+  if (cc3_histogram_scanline_start (h_pkt) != 0) {
+    while (cc3_pixbuf_read_rows (img.pix, 1)) {
+      // This does the HSV conversion 
+      // cc3_rgb2hsv_row(img.pix,img.width);
+      cc3_histogram_scanline (&img, h_pkt);
+    }
+  }
+  cc3_histogram_scanline_finish (h_pkt);
+
+  free (img.pix);
+  return;
+}
