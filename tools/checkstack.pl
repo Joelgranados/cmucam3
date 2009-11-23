135061b97c30a56c60691b7291fe9173f57d220c
goodea <goodea@111db651-790d-0410-a044-b0abe44a5820>
2009-11-23 06:29:10 +0000

    Update checkstack for thumb
    git-svn-id: https://cc3.svn.sourceforge.net/svnroot/cc3/trunk@565 111db651-790d-0410-a044-b0abe44a5820

--- tools/checkstack.pl
+++ tools/checkstack.pl
@@ -37,6 +37,9 @@ my (@stack, $re, $x, $xs);
 	if ($arch eq 'arm') {
 		#c0008ffc:	e24dd064	sub	sp, sp, #100	; 0x64
 		$re = qr/.*sub.*sp, sp, #(([0-9]{2}|[3-9])[0-9]{2})/o;
+	} elsif ($arch eq 'thumb') {
+		#d7a:       b083            sub     sp, #12
+		$re = qr/.*sub.*sp, #([0-9]+)/o;
 	} elsif ($arch =~ /^i[3456]86$/) {
 		#c0105234:       81 ec ac 05 00 00       sub    $0x5ac,%esp
 		$re = qr/^.*[as][du][db]    \$(0x$x{1,8}),\%esp$/o;
@@ -127,7 +130,7 @@ while (my $line = <STDIN>) {
 			$intro .= '	';
 			$padlen -= 8;
 		}
-		next if ($size < 100);
+#		next if ($size < 100);
 		push @stack, "$intro$size\n";
 	}
 }
