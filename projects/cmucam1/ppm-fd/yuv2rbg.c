0501d866567b2f48c32addd8ddb7c078ed7a957d
anthony_rowe <anthony_rowe@111db651-790d-0410-a044-b0abe44a5820>
2010-02-17 16:44:49 +0000

    added interger yuv conversion
    git-svn-id: https://cc3.svn.sourceforge.net/svnroot/cc3/trunk@617 111db651-790d-0410-a044-b0abe44a5820

--- projects/cmucam1/ppm-fd/yuv2rbg.c
+++ projects/cmucam1/ppm-fd/yuv2rbg.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int clip(int x)
+{
+if(x>255) x=255;
+if(x<0) x=0;
+return x;
+}
 
 int main()
 {
 FILE *fp,*out_fp,*mono_fp;
 int x,y,x_size,y_size,depth;
-int yc,uc,vc,r,g,b;
+int yc,uc,vc,r,g,b,C,D,E;
 char type[32];
 
 out_fp=fopen("rgb.ppm","w" );
@@ -29,15 +35,23 @@ for(y=0; y<y_size; y++ )
 	//b = 1.164*(yc - 16) + 2.018*(uc - 128);
 	//g = 1.164*(yc - 16) - 0.813*(vc - 128) - 0.391*(uc - 128);
 	//r = 1.164*(yc - 16) + 1.596*(vc - 128);
-	r = yc + 1.403*vc;
-	g = yc - 0.344*uc - 0.714 * vc;
-	b = yc + 1.770 * uc;
-	if (r > 255) r = 255;
-   	if (g > 255) g = 255;
-   	if (b > 255) b = 255;
-   	if (r < 0) r = 0;
-   	if (g < 0) g = 0;
-   	if (b < 0) b = 0;
+	//r = yc + 1.403*vc;
+	//g = yc - 0.344*uc - 0.714 * vc;
+	//b = yc + 1.770 * uc;
+	//r = yc + 1.402 * (uc-128);
+	//g = yc - 0.34414 * (vc-128) - 0.71414 * (uc-128);
+	//b = yc + 1.772 * (vc-128);
+	C= yc - 16;
+	D= uc - 128;
+	E= vc - 128;
+
+	r = ( 298 * C           + 409 * E + 128) >> 8;
+	g = ( 298 * C - 100 * D - 208 * E + 128) >> 8;
+	b = ( 298 * C + 516 * D           + 128) >> 8;
+
+	r=clip(r);
+	g=clip(g);
+	b=clip(b);
 
 	fprintf( out_fp,"%c%c%c",r,g,b);
 	fprintf( mono_fp,"%c%c%c",yc,yc,yc);
